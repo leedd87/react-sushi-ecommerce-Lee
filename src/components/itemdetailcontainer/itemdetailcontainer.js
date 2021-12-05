f94790c62f48606b530378f361d4ae68f60e3928
Lee David <leedav87@gmail.com>
2021-12-05 20:10:05 -0300

    ultima revision entrega final

--- src/components/itemdetailcontainer/itemdetailcontainer.js
+++ src/components/itemdetailcontainer/itemdetailcontainer.js
@@ -3,13 +3,12 @@ import { useParams } from 'react-router-dom';
 import ItemDetail from '../itemdetail/itemDetail';
 import { getFirestore } from '../../firebase';
 import { doc, getDoc } from 'firebase/firestore';
-// import Loader from '../loader/loader'
+
 
 const ItemDetailContainer = () => {
 
     const { detailId } = useParams();
     const [productos, setProductos] = useState({});
-    // const [loadingTwo, setLoadingTwo] = useState(true)
 
     useEffect(() => {
         const db = getFirestore();
@@ -28,11 +27,11 @@ const ItemDetailContainer = () => {
         <>
             <div className='container-fluid row justify-content-center'>
                 {
-                    // productos ?
-                    // loadingTwo ? <Loader /> :
+
+
                     <ItemDetail
                         item={productos} />
-                    // : "Loading..."
+
                 }
             </div>
         </>
